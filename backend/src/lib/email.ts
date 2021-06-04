cb7d889dd0106cbd46b0412d7e7258831f944b68
guilleeh <guilleeh@uci.edu>
2021-06-04 12:38:19 -0700

    add another var

--- backend/src/lib/email.ts
+++ backend/src/lib/email.ts
@@ -5,7 +5,7 @@ const sendEmail = async (to: string, subject: string, text: string) => {
   const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
-    secure: false,
+    secure: process.env.MAIL_SECURE,
     auth: {
       user: process.env.MAIL_USERNAME,
       pass: process.env.MAIL_PASSWORD,
